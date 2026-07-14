6f9aea686a6fb0539045fadc3b8633d83c96391a
VVIGNESH2007 <v.vignesh.com.in@gmail.com>
2026-07-14 12:16:53 +0530

    Create 78-Subsets

---
+++ src/78-Subsets/78-Subsets.java
@@ -0,0 +1,17 @@
+// Last updated: 7/14/2026, 12:16:52 PM
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(res, new ArrayList<>(), nums, 0);
+        return res;
+    }
+    
+    private void backtrack(List<List<Integer>> res, List<Integer> path, int[] nums, int start) {
+        res.add(new ArrayList<>(path));
+        for (int i = start; i < nums.length; i++) {
+            path.add(nums[i]);
+            backtrack(res, path, nums, i + 1);
+            path.remove(path.size() - 1);
+        }
+    }
+}
