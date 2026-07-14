c073dc9bfe4fda246b476b75b7e653a19dba0023
VVIGNESH2007 <v.vignesh.com.in@gmail.com>
2026-07-14 12:16:58 +0530

    Create 70-ClimbingStairs

---
+++ src/70-ClimbingStairs/70-ClimbingStairs.java
@@ -0,0 +1,19 @@
+// Last updated: 7/14/2026, 12:16:58 PM
+class Solution {
+    public int climbStairs(int n) {
+        if(n==0){
+            return 1;
+        }
+        if(n<3){
+            return n;
+        }
+        int []arr=new int[n+1];
+        arr[0]=0;
+        arr[1]=1;
+        arr[2]=2;
+        for(int i=3;i<=n;i++){
+            arr[i]=arr[i-1]+arr[i-2];
+        }
+        return arr[n];
+    }
+}
