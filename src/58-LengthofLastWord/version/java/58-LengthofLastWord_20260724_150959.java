a3dc5804b2b58df85ddbce009154b3efd6b261ce
VVIGNESH2007 <v.vignesh.com.in@gmail.com>
2026-07-24 15:09:59 +0530

    Create 58-LengthofLastWord

---
+++ src/58-LengthofLastWord/version/java/58-LengthofLastWord_20260724_150959.java
@@ -0,0 +1,24 @@
+// Last updated: 7/24/2026, 3:09:59 PM
+1class Solution {
+2    public int lengthOfLastWord(String s) {
+3        Stack<String> st=new Stack<>();
+4        int i=0;
+5        int n=s.length();
+6        while(s.charAt(i)==' '){
+7            i++;
+8        }
+9        int j=i;
+10        while(j<n){
+11            while(j!=n&&s.charAt(j)!=' '){
+12                j++;
+13            }
+14            st.push(s.substring(i,j));
+15            j++;
+16            i=j;
+17        }
+18        while(st.peek().length()==0){
+19            st.pop();
+20        }
+21        return st.peek().length();
+22    }
+23}
